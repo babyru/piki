73eac8389bc043485071a0c7c82aa87f9eb507fc
Mailer <kailaasa.mailer@gmail.com>
2024-09-25 21:07:40 +0300

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = async () => {
+  return (
+    <div>
+      <p>Home</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UpdateTransformationsPage = () => {
+  return <div>UpdateTraansformationsPage</div>;
+};
+
+export default UpdateTransformationsPage;
